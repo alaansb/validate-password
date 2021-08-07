904eb5c45f32e11766a92051e01f8b652af8d2e8
alaansb <alansouzaboaventura@gmail.com>
2021-08-06 21:20:17 -0300

    Init config

---
+++ src/test/java/com/validatepassword/ValidatePasswordApplicationTests.java
@@ -0,0 +1,13 @@
+package com.validatepassword;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ValidatePasswordApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
